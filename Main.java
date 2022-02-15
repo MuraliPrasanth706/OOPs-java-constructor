8475255da5667ac7e389d475b95bef6504a2e6e3
MuraliPrasanth <83390114+MuraliPrasanth706@users.noreply.github.com>
2022-02-15 07:23:14 +0000

    diffrent constructor

--- Main.java
+++ Main.java
@@ -1,14 +1,18 @@
  class Calc{
    int num1,num2,result;
-  public Calc(){
+  public Calc(){ //not taking any parameter
     num1 =9;
     num2 =10;
   }
+   public Calc(int k){  //Taking the parameter
+     num1= k;
+     num2 =k;
+   }
  }
 
 class Main {
   public static void main(String[] args) {
-    Calc obj = new Calc();
+    Calc obj = new Calc(8);
     System.out.println(obj.num1);
     }
 }
